4999e09f63a5c9401e103a75a943b30cd76a5670
htmltableoptionswhitelist <winnerstourplantao@gmail.com>
2016-08-14 14:45:15 -0300

    lbjb
    nm nm

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: htmltableoptionswhitelist
+# about: htmltableoptionswhitelist
+# version: 1
+# authors: htmltableoptionswhitelist
+# url: https://github.com/htmltableoptionswhitelist/htmltableoptionswhitelist/
+
+register_asset "javascript/progress.js.es6", :server_side
